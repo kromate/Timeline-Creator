d46fff00257965fd75e07fa8ac9682e942cf21c9
kromate <akpananthony33@gmail.com>
2022-03-01 18:01:55 +0100

    started the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,34 @@
 <template>
-<one/>
+<div class="w-full h-screen bg-orange-300">
+  <div class="container mx-auto flex flex-col justify-center items-center h-full text-center">
+    <h1 class="md:text-6xl text-4xl font-bold text-white title tracking-wider mb-4">Welcome to Timelinx</h1>
+    <p class="md:text-2xl text-lg">
+      Create Timeline for your projects, activities and milestones.
+    </p>
+
+<div class="mt-5 w-full">
+    <button class="py-2 px-7 text-xl  border-4 rounded-lg border-purple-700 text w-[25rem] max-w-[75%]">
+      Get Started
+    </button>
+</div>
+
+  </div>
+
+</div>
 </template>
 
 <script lang="ts">
-import One from '~/components/timeline/One.vue'
 
 export default{
   name: 'IndexPage',
-  components: { One },
 }
 </script>
+
+<style scoped>
+    .title {
+            text-shadow: -2px 2px 0 #727cb6,
+                          2px 2px 0 #727cb6,
+                         2px -2px 0 #727cb6,
+                        -2px -2px 0 #727cb6;
+        }
+</style>
---
+++ pages/setup.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-full h-screen bg-orange-200">
+        <div class="container mx-auto flex justify-center items-center h-full">
+        <h1 class="text-6xl font-bold text-white">Setup</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'SetupPage',
+}
+</script>
+
+<style>
+
+</style>
