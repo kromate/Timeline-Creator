464dfb1851311b9e59327a48f31ce20534ecce65
kromate <akpananthony33@gmail.com>
2022-03-06 03:06:00 +0100

    color scheme

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <template>
-<div class="w-full h-screen bg-orange-300">
+<div class="w-full h-screen bg-slate-800">
   <div class="container mx-auto flex flex-col justify-center items-center h-full text-center">
     <h1 class="md:text-6xl text-4xl font-bold text-white title tracking-wider mb-4">Welcome to Timelinx</h1>
-    <p class="md:text-2xl text-lg">
+    <p class="md:text-2xl text-lg text-orange-400">
       Create Timeline for your projects, activities and milestones.
     </p>
 
 <div class="mt-12 w-full flex flex-col justify-center items-center">
-    <nuxt-link to="/" class=" btn border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white">
+    <nuxt-link to="/" class=" btn border-white text-white ">
       Get Started
       <i class="fas fa-star"></i>
     </nuxt-link >
-    <nuxt-link to="/" class=" btn border-yellow-700 text-yellow-700 hover:bg-yellow-700 hover:text-white mt-4">
+    <nuxt-link to="/" class=" btn border-white text-white  mt-4">
       Demo
       <i class="fas fa-book"></i>
     </nuxt-link >
-    <nuxt-link to="/" class=" btn border-green-700 text-green-700 hover:bg-green-700 hover:text-white mt-4">
+    <nuxt-link to="/" class=" btn border-white text-white  mt-4">
       Github
       <i class="fas fa-laptop"></i>
     </nuxt-link >
