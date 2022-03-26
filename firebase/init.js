3b86eba3cf5b143d1622e210c2adf242c400f067
kromate <akpananthony33@gmail.com>
2022-03-26 22:34:26 +0100

    feat: env

---
+++ firebase/init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyCNYlmOMKDtL7amcDHV2pzZltWPf6tfaTc',
+	authDomain: 'timelinx.firebaseapp.com',
+	projectId: 'timelinx',
+	storageBucket: 'timelinx.appspot.com',
+	messagingSenderId: '104278527964',
+	appId: '1:104278527964:web:5bc248cc1d3288e5218919'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
