8ab9cadbf05a1e52f2563f9ff79d70654cedfb39
kromate <akpananthony33@gmail.com>
2022-03-27 19:52:24 +0100

    feat: update

--- components/AlertBox.vue
+++ components/AlertBox.vue
@@ -1,6 +1,8 @@
 <template>
-	<div class="bg fixed top-4 left-4 h-16 min-w-[10rem]">
-		Hello world
+	<div class="bg-inverse text-inverse fixed top-8	 left-4 h-16 min-w-[10rem] max-w-[60%] p-2 pl-3">
+		<div class="h-full w-1 bg-green-600 absolute left-0 top-0"/>
+
+		<p>Hello World and konnichiwa to the rest of you all</p>
 	</div>
 </template>
 
