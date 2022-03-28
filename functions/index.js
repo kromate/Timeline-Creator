3f08afedda81b13962abf54ace46039c14758cdc
kromate <akpananthony33@gmail.com>
2022-03-28 19:33:44 +0100

    feat: update

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate((user) => {
+	// for background triggers you must return a value/promise
+	return admin.firestore().collection('users').doc(user.uid).set({
+		id: user.uid,
+		email: user.email,
+		phone: '',
+	});
+});
+
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+	const doc = admin.firestore().collection('users').doc(user.uid);
+	return doc.delete();
+});
