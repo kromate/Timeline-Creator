d2a0e03bdfa0c01525851fc4094c9008d5322b81
kromate <akpananthony33@gmail.com>
2022-03-30 02:16:32 +0100

    feat: update

--- pages/timeline/_id.vue
+++ pages/timeline/_id.vue
@@ -35,7 +35,7 @@
 			</div>
 		</div>
 
-		<div v-else class="">
+		<div v-else class="grid grid-cols-1 place-content-center place-items-center">
 			<lottie-player src="https://assets2.lottiefiles.com/packages/lf20_4owMZE.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
 			<h3 class="text text-center">We would not find your requested timeline. If someone sent this to you beat them up 😼, you can also create your by click the button below</h3>
 			<nuxt-link to="/setup" class=" btn max-w-[75%] mt-4 justify-center items-center flex">
