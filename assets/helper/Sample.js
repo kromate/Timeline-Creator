c07c6aa3614d923cb6dde2413eee1d0469648648
kromate <akpananthony33@gmail.com>
2022-04-07 23:16:43 +0100

    --- Auto-Git Commit ---

--- assets/helper/Sample.js
+++ assets/helper/Sample.js
@@ -1,21 +1,21 @@
 const Types = {
-    YEAR: 'yearly',
-    MONTH: 'monthly',
-    WEEK: 'weekly',
-    DAY: 'daily',
+	YEAR: 'yearly',
+	MONTH: 'monthly',
+	WEEK: 'weekly',
+	DAY: 'daily',
 }
 
 Object.freeze(Types);
 
 export const Sample = {
-    Title: "Title Of the Timeline",
-    Description: "Description of the Timeline",
-    Type: Types.YEAR,
-    Data: [
-        {
-            Date: "2020-01-01",
-            Time: "00:00",
-            Details: "Details of the event",
-        }
-    ]
+	Title: 'Title Of the Timeline',
+	Description: 'Description of the Timeline',
+	Type: Types.YEAR,
+	Data: [
+		{
+			Date: '2020-01-01',
+			Time: '00:00',
+			Details: 'Details of the event',
+		}
+	]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  mode: 'jit',
-  darkMode:"class",
-    content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	mode: 'jit',
+	darkMode:'class',
+	content: [
+		'./components/**/*.{js,vue,ts}',
+		'./layouts/**/*.vue',
+		'./pages/**/*.vue',
+		'./plugins/**/*.{js,ts}',
+		'./nuxt.config.{js,ts}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
